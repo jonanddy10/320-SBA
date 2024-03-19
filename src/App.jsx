38be403b3114f1b6fc6c792aa869e7f2ea435173
jonanddy10 <jonsairmail@icloud.com>
2024-03-19 08:10:19 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, createContext, useContext } from 'react'
 import './App.css'
   // main components
   import Header from './components/Header'
   import Hourly from './components/Hourly'
   import Weekly from './components/Weekly'
-  import Background from './components/Background'
+
+const WeatherContext = createContext();
 
 function App() {
-  const [data, setData] = useState(null)
-  const [headerData, setHeaderData] = useState({})
-
-  const URL = 'https://api.weather.gov/gridpoints/SEW/124,69/forecast'
-
-  // retrieve ... and store data 
-  useEffect(() => {
-    const fetchData = async () => {
-    try {
-      const result = await fetch(URL)
-      const json = await result.json()
-      
-      setData([...json.properties.periods])
-      getHeader()
-      
-    } catch (error) {
-      console.log(`There's been an error connecting to the API`)
-    }
-  fetchData();
-  }
-}, [])
 
   return (
     <>
@@ -46,12 +26,14 @@ export default App
 function getHeader() {
   if(data !== null){
 
-  setHeaderData({
+  setWeatherData({
     shift: data.name, 
     low: data.temperature,
     now: data.temperature,
-    high: data.temperature
+    high: data.temperature,
+    weekly: [],
+    hourly: [],
   })
-  console.log(headerData)
+  console.log(weatherData)
   }
 }
--- src/components/WeatherProvider.jsx
+++ src/components/WeatherProvider.jsx
@@ -1,15 +1,48 @@
 import React from 'react'
 
-function WeatherProvider() {
-    const[weatherData, setWeatherData] = useState(null)
+function WeatherProvider({children}) {
+  const[weatherData, setWeatherData] = useState(null)
 
-    useEffect(() => {
-        fetchWeatherData().then(data => setWeatherData(data));
-    }, []);
+  const [data, setData] = useState(null)
+  // const [headerData, setHeaderData] = useState({})
+
+  const URL = 'https://api.weather.gov/gridpoints/SEW/124,69/forecast'
+
+  // retrieve ... and store data 
+  useEffect(() => {
+    const fetchData = async () => {
+    try {
+      const result = await fetch(URL)
+      const json = await result.json()
+      
+      setData([...json.properties.periods])
+      getHeader()
+      
+    } catch (error) {
+      console.log(`There's been an error connecting to the API`)
+    }
+  fetchData();
+  }
+}, [])
+
+function getHeader() {
+  if(data !== null){
+
+  setWeatherData({
+    shift: data.name, 
+    low: data.temperature,
+    now: data.temperature,
+    high: data.temperature,
+    weekly: [],
+    hourly: [],
+  })
+  console.log(weatherData)
+  }
+}
 
   return (
     <WeatherContext.Provider value={weatherData}>
-        <h2>WeatherProvider, children here:</h2>
+        ~WeatherProvider children here~
     </WeatherContext.Provider >
   )
 }
