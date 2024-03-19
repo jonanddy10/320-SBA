e35a097634eef8de6ffb2d518bb66910d3a2ecc0
jonanddy10 <jonsairmail@icloud.com>
2024-03-19 08:42:30 -0700

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Weather App Pro +
 
-This project is local weather app for Seattle created using React.
+# This project is local weather app for Seattle created using React.
+
+### This react application will provide the current temperature in Seattle (F). When clicking on the 'Hourly' or 'Weekly' option a 'new screen' will display the associated data.
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+// This react application will provide the current temperature in Seattle (F). 
+// When clicking on the 'Hourly' or 'Weekly' option a 'new screen' will display the associated data. 
+
 import { useState, useEffect, createContext, useContext } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+
   // main components
   import Header from './components/Header'
   import HourlyForecast from './components/HourlyForecast'
   import WeeklyForecast from './components/WeeklyForecast'
+  import WeatherProvider from './components/WeatherProvider';
 
 const WeatherContext = createContext();
 
